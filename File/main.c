d77e5e6a13b99d597247a5cf5f4d3baf5f5bd2d6
lyh20161104592 <lyh20161104592@Mac-20.local>
2017-06-13 09:51:35 +0800

    weiwancheng

--- File/main.c
+++ File/main.c
@@ -6,10 +6,16 @@
 //  Copyright © 2017年 lyh20161104592. All rights reserved.
 //
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include<stdio.h>
+int main(){
+    FILE *fr;
+    FILE *fw;
+    fr=fopen("//Users//lyh20161104592//Desktop//File//input.txt","r+");
+    fw=fopen("//Users//lyh20161104592//Desktop//File//output.txt","w");
+    int a,b,c;
+    fscanf(fr,"%d %d",&a,&b);
+    c=a+b;
+    printf("%d+%d=%d \n",a,b,c);
+    fprintf(fw,"%d+%d=%d \n",a,b,c);
     return 0;
 }
