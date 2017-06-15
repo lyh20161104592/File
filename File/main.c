4847c3d3ff804859e5544dcfff58bc5250aaa5f7
lyh20161104592 <lyh20161104592@Mac-20.local>
2017-06-15 15:51:27 +0800

    改正1
    2017.6.15

--- File/main.c
+++ File/main.c
@@ -7,15 +7,48 @@
 //
 
 #include<stdio.h>
-int main(){
-    FILE *fr;
-    FILE *fw;
-    fr=fopen("//Users//lyh20161104592//Desktop//File//input.txt","r+");
-    fw=fopen("//Users//lyh20161104592//Desktop//File//output.txt","w");
-    int a,b,c;
-    fscanf(fr,"%d %d",&a,&b);
-    c=a+b;
-    printf("%d+%d=%d \n",a,b,c);
-    fprintf(fw,"%d+%d=%d \n",a,b,c);
-    return 0;
+int main()
+{
+    int a[20];
+    int n,k,j,i,temp;
+    FILE *fp1;
+    FILE *fp2;
+    fp1=fopen("//Users//lyh20161104592//Desktop//File//paixu.txt","r+");
+    fp2=fopen("//Users//lyh20161104592//Desktop//File//paixuout.txt","w+");
+    if(fp1==NULL)
+    {
+      printf("打开文件错误，要打开的文件可能不存在\n");
+    }
+    else
+    {
+        while(fscanf(fp1,"%d",&n)!=EOF)
+        {
+            for(i=0;i<n;i++)
+            {
+                fscanf(fp1,"%d",&a[i]);
+            }
+            for(j=0;j<n-1;j++)
+            {
+                for(k=0;k<n-1-j;k++)
+                {
+                    if(a[k]>a[k+1])
+                    {
+                        temp=a[k];
+                        a[k]=a[k+1];
+                        a[k+1]=temp;
+                    }
+                }
+            }
+            for(k=0;k<n;k++)
+            {
+                printf("%d ",a[k]);
+                fprintf(fp2,"%d ",a[k]);
+            }
+            fprintf(fp2,"\n");
+            printf("\n");
+        }
+             fclose(fp1);
+             fclose(fp2);
+    }
+             return 0;
 }
